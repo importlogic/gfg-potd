55be65909c862af093568cc01e0103a1c669d68f
Manas Rawat <manasrawat.cse@gmail.com>
2023-02-10 11:14:49 +0530

    10-02-2023

---
+++ 2023/February/10-02-2023.cpp
@@ -0,0 +1,53 @@
+/*
+
+Author : Manas Rawat
+Date : 10/02/2023
+Problem : Balloons Everywhere
+Difficulty : Easy
+Problem Link : https://practice.geeksforgeeks.org/problems/45fa306a9116332ece4cecdaedf50f140bd252d4/1
+
+*/
+
+
+
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int maxInstance(string s){
+        map<char,int> mp;
+        
+        for(auto i : s) ++mp[i];
+        
+        mp['l'] >>= 1; //dividing by 2
+        mp['o'] >>= 1;
+        
+        int ans = min({mp['b'], mp['a'], mp['l'], mp['o'], mp['n']});
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.maxInstance(s)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
---
+++ README.md
@@ -0,0 +1,7 @@
+# Geeks for Geeks Problem of the Day Repository
+This repository is a collection of C++ codes for Geeks for Geeks problem of the Day.
+
+## Code of Conduct
+The solutions to the problems given here are just for reference only. Please avoid copying the code and trying doing them on your own by taking help from this repository.
+
+### Happy Coding

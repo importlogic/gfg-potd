230bbe1653fbd3884015571413903cce88f3cc4b
Manas Rawat <manasrawat.cse@gmail.com>
2023-05-12 07:36:14 +0530

    12-05-2023

---
+++ 2023/05 - May/12-05-2023.cpp
@@ -0,0 +1,66 @@
+/*
+
+Author : Manas Rawat
+Date : 12/05/2023
+Problem : Array Operations
+Difficulty : Easy
+Problem Link : https://practice.geeksforgeeks.org/problems/3a93b6a25a7b88e4c80a1fee00898fd8022eb108/1
+Video Solution : https://youtu.be/tVYMJbrgSOY
+
+*/
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int arrayOperations(int n, vector<int> &arr) {
+        int ans = 0;
+        int current = 0;
+        
+        for(int i = 0; i < n; i++){
+            if(arr[i] == 0){
+                ans += current > 0;
+                current = 0;
+                continue;
+            }
+            
+            ++current;
+        }
+        
+        ans += current > 0;
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        
+        int n;
+        cin>>n;
+        
+        
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        
+        Solution obj;
+        int res = obj.arrayOperations(n, arr);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends

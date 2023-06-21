1a03c697324e3a988816c5a79a147e8f3ab0cf38
Manas Rawat <manasrawat.cse@gmail.com>
2023-06-21 10:13:10 +0530

    21-06-2023 updated

--- 2023/06 - June/21-06-2023.cpp
+++ 2023/06 - June/21-06-2023.cpp
@@ -20,7 +20,7 @@ class Solution {
     int sumOfNaturals(int n) {
         const int mod = 1e9 + 7;
         long long ans = (n * (n + 1LL)) % mod;
-        ans = (ans / 2LL) % mod;
+        ans = (ans * 500000004LL) % mod;
         return ans;
     }
 };

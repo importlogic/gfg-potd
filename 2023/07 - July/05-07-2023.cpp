bb3ff6c4cd9aaebe56b4d3d845c60fd6baa711f1
Manas Rawat <manasrawat.cse@gmail.com>
2023-07-05 09:46:30 +0530

    05-07-2023

---
+++ 2023/07 - July/05-07-2023.cpp
@@ -0,0 +1,82 @@
+/*
+
+Author : Manas Rawat
+Date : 05/07/2023
+Problem : Stock buy and sell II
+Difficulty : Medium
+Problem Link : https://practice.geeksforgeeks.org/problems/stock-buy-and-sell2615/1
+Video Solution : https://youtu.be/czsdxUkvRVg
+
+*/
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int stockBuyAndSell(int n, vector<int> &prices) {
+        int ans = 0;
+        
+        for(int i = 0; i < n - 1; i++){
+            if(prices[i + 1] > prices[i]){
+                ans += prices[i + 1] - prices[i];
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> prices(n);
+        Array::input(prices,n);
+        
+        Solution obj;
+        int res = obj.stockBuyAndSell(n, prices);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends

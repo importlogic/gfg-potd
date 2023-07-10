8e4973960979a4cf1bec1589963a46c05e968bef
Manas Rawat <manasrawat.cse@gmail.com>
2023-07-10 11:24:21 +0530

    10-07-2023

---
+++ 2023/07 - July/10-07-2023.cpp
@@ -0,0 +1,63 @@
+/*
+
+Author : Manas Rawat
+Date : 10/07/2023
+Problem : Transpose of Matrix
+Difficulty : Easy
+Problem Link : https://practice.geeksforgeeks.org/problems/transpose-of-matrix-1587115621/1
+Video Solution : https://youtu.be/99tVbkgMnUw
+
+*/
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{   
+    public:  
+    //Function to find transpose of a matrix.
+    void transpose(vector<vector<int> >& matrix, int n)
+    { 
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > matrix(n,vector<int>(n)); 
+
+        for(int i=0; i<n; i++)
+        {
+            for( int j=0; j<n; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        ob.transpose(matrix,n);
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+                cout<<matrix[i][j]<<" ";
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends

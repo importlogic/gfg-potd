4e0fe537292fd4bef11ab20e31d2c0807894f863
Manas Rawat <manasrawat.cse@gmail.com>
2023-08-25 14:38:27 +0530

    25-08-2023

---
+++ 2023/08 - August/25-08-2023.cpp
@@ -0,0 +1,61 @@
+/*
+
+Author : Manas Rawat
+Date : 25/08/2023
+Problem : Palindrome String
+Difficulty : Easy
+Problem Link : https://practice.geeksforgeeks.org/problems/palindrome-string0817/1
+Video Solution : NA
+
+*/
+
+
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    int n = S.size();
+	    
+	    for(int i = 0; i < n / 2; i++){
+	        if(S[i] != S[n - i - 1])
+	            return 0;
+	    }
+	    
+	    return 1;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends

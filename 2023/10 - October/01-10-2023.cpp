664bd8e4a57ae61afb0fa2e17a8b9c9da1199537
Manas Rawat <manasrawat.cse@gmail.com>
2023-10-01 11:02:09 +0530

    01-10-2023

---
+++ 2023/10 - October/01-10-2023.cpp
@@ -0,0 +1,79 @@
+/*
+
+Author : Manas Rawat
+Date : 01/10/2023
+Problem : Boundary traversal of matrix
+Difficulty : Easy
+Problem Link : https://practice.geeksforgeeks.org/problems/boundary-traversal-of-matrix-1587115620/1
+Video Solution : NA
+
+*/
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+class Solution
+{   
+    public:
+    //Function to return list of integers that form the boundary 
+    //traversal of the matrix in a clockwise manner.
+    vector<int> boundaryTraversal(vector<vector<int> > matrix, int n, int m) 
+    {
+        vector<int> ans;
+        
+        for(int i = 0; i < m; i++){
+            ans.push_back(matrix[0][i]);
+        }
+        
+        for(int i = 1; i < n; i++){
+            ans.push_back(matrix[i][m - 1]);
+        }
+        
+        if(n > 1)
+            for(int i = m - 2; i > -1; i--){
+                ans.push_back(matrix[n - 1][i]);
+            }
+        
+        if(m > 1)
+            for(int i = n - 2; i > 0; i--){
+                ans.push_back(matrix[i][0]);
+            }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int n,m;
+        cin>>n>>m;
+        vector<vector<int> > matrix(n); 
+
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(m, 0);
+            for( int j=0; j<m; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.boundaryTraversal(matrix, n, m);
+        for (int i = 0; i < result.size(); ++i)
+                cout<<result[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

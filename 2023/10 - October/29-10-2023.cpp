fcac9adb256acca26b47c39a8b20cf7c62b27ceb
Manas Rawat <manasrawat.cse@gmail.com>
2023-10-29 11:15:15 +0530

    29-10-2023

---
+++ 2023/10 - October/29-10-2023.cpp
@@ -0,0 +1,55 @@
+/*
+
+Author : Manas Rawat
+Date : 29/10/2023
+Problem : Check whether K-th bit is set or not
+Difficulty : Easy
+Problem Link : https://practice.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not-1587115620/1
+Video Solution : NA
+
+*/
+
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution
+{
+    public:
+    // Function to check if Kth bit is set or not.
+    bool checkKthBit(int n, int k)
+    {
+        return (n >> k) & 1;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{
+	int t;
+	cin>>t;//taking testcases
+	while(t--)
+	{
+	    long long n;
+	    cin>>n;//input n
+	    int k;
+	    cin>>k;//bit number k
+	    Solution obj;
+	    if(obj.checkKthBit(n, k))
+	        cout << "Yes" << endl;
+	    else
+	        cout << "No" << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends

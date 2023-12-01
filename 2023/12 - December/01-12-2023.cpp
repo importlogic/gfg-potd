b48a6f8c2a8cc3ade2fa9328501558e92d08ec03
Manas Rawat <manasrawat.cse@gmail.com>
2023-12-01 06:24:38 +0530

    01-12-2023

---
+++ 2023/12 - December/01-12-2023.cpp
@@ -0,0 +1,135 @@
+/*
+
+Author : Manas Rawat
+Date : 01/12/2023
+Problem : Check whether BST contains Dead End
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/check-whether-bst-contains-dead-end/1
+Video Solution : https://youtu.be/i_TgruIvTww
+
+*/
+
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void insert(Node ** tree, int val)
+{
+    Node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = new Node(val);
+        *tree = temp;
+        return;
+    }
+
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+
+}
+
+
+
+
+int getCountOfNode(Node *root, int l, int h)
+{
+
+    if (!root) return 0;
+
+
+    if (root->data == h && root->data == l)
+        return 1;
+
+    if (root->data <= h && root->data >= l)
+         return 1 + getCountOfNode(root->left, l, h) +
+                    getCountOfNode(root->right, l, h);
+
+    else if (root->data < l)
+         return getCountOfNode(root->right, l, h);
+
+
+    else return getCountOfNode(root->left, l, h);
+}
+
+
+// } Driver Code Ends
+/*The Node structure is
+struct Node {
+int data;
+Node * right, * left;
+};*/
+
+/*You are required to complete below method */
+class Solution{
+  public:
+    bool helper(Node * node, int MIN, int MAX){
+        if(!node)
+            return 0;
+            
+        if(node -> data == MIN and node -> data == MAX)
+            return 1;
+            
+        return helper(node -> left, MIN, node -> data - 1) or helper(node -> right, node -> data + 1, MAX);    
+    }
+    
+    bool isDeadEnd(Node *root)
+    {
+        return helper(root, 1, 1e8);
+    }
+};
+
+//{ Driver Code Starts.
+// bool isDeadEnd(Node *root);
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Node *root;
+        Node *tmp;
+    //int i;
+
+        root = NULL;
+
+        int N;
+        cin>>N;
+        for(int i=0;i<N;i++)
+        {
+            int k;
+            cin>>k;
+            insert(&root, k);
+
+        }
+
+     Solution ob;
+     cout<<ob.isDeadEnd(root);
+     cout<<endl;
+    }
+}
+
+
+// } Driver Code Ends

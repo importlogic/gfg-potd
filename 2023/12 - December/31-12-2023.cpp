db59134286eaf195b8ac432b18f0f0375262cc6b
Manas Rawat <manasrawat.cse@gmail.com>
2023-12-31 11:16:31 +0530

    31-12-2023 updated

--- 2023/12 - December/31-12-2023.cpp
+++ 2023/12 - December/31-12-2023.cpp
@@ -22,22 +22,25 @@ class Solution {
     public:
     int isPossible(int N , int coins[]) 
     {
-        vector<vector<int>> dp(N + 1, vector<int> (2025, 0));
+        int MAX = accumulate(coins, coins + N, 0LL) + 1;
         
-        for(int i = 20; i < 2025; i += 20)
+        vector<vector<int>> dp(N + 1, vector<int> (MAX, 0));
+        
+        for(int i = 20; i < MAX; i += 20)
             dp[N][i] = 1;
-        for(int i = 24; i < 2025; i += 24)
+        for(int i = 24; i < MAX; i += 24)
             dp[N][i] = 1;
-        dp[N][2024] = 1;
+        if(2024 < MAX)
+            dp[N][2024] = 1;
         
         for(int i = N - 1; i > -1; i--){
-            for(int j = 0; j < 2025; j++){
+            for(int j = 0; j < MAX; j++){
                 int take, notake;
                 take = notake = 0;
                 
                 notake = dp[i + 1][j];
                 
-                if(coins[i] + j < 2025)
+                if(coins[i] + j < MAX)
                     take = dp[i + 1][j + coins[i]];
                     
                 dp[i][j] = max(take, notake);

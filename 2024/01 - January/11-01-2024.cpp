00ed42f4b053f20bb72a9fed339f44178a2b82ae
Manas Rawat <manasrawat.cse@gmail.com>
2024-01-11 09:35:25 +0530

    11-01-2024

---
+++ 2024/01 - January/11-01-2024.cpp
@@ -0,0 +1,96 @@
+/*
+
+Author : Manas Rawat
+Date : 11/01/2024
+Problem : Remove K Digits
+Difficulty : Medium
+Problem Link : https://www.geeksforgeeks.org/problems/remove-k-digits/1
+Video Solution : https://youtu.be/fYopLz9nmes
+
+*/
+
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    string removeKdigits(string S, int K) {
+        pair<int, int> last = {0, K};
+        int n = S.size();
+        int index = 0;
+        
+        while(index < n){
+            if(S[index] != '0'){
+                if(K)
+                    --K;
+                else
+                    break;
+                    
+                ++index;
+            }
+            else{
+                while(index < n and S[index] == '0'){
+                    ++index;
+                }
+                
+                last = {index, K};
+            }
+        }
+        
+        stack<int> num;
+        int start = last.first;
+        K = last.second;
+        
+        for(int i = start; i < n; i++){
+            while(num.size() and num.top() > (S[i] - '0') and K){
+                --K;
+                num.pop();
+            }
+            
+            num.push(S[i] - '0');
+        }
+        
+        while(num.size() and K--){
+            num.pop();
+        }
+        
+        string ans = "";
+        
+        while(num.size()){
+            ans += to_string(num.top());
+            num.pop();
+        }
+        
+        reverse(ans.begin(), ans.end());
+        
+        if(ans == "")
+            ans = "0";
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        int K;
+        cin >> K;
+        Solution obj;
+        cout << obj.removeKdigits(S, K) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends

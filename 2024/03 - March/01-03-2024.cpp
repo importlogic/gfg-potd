e54399b1cc89bd5c0588ed726550184d6b864d88
Manas Rawat <manasrawat.cse@gmail.com>
2024-03-01 07:21:06 +0530

    01-03-2024

---
+++ 2024/03 - March/01-03-2024.cpp
@@ -0,0 +1,86 @@
+/*
+
+Author : Manas Rawat 
+Date : 01/03/2024
+Problem : Peak element
+Difficulty : Medium 
+Problem Link : https://www.geeksforgeeks.org/problems/peak-element/1
+Video Solution : NA
+
+*/
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/* The function should return the index of any
+   peak element present in the array */
+
+// arr: input array
+// n: size of array
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n)
+    {
+        int low = 0;
+        int high = n;
+       
+        while(low < high - 1) {
+            int mid = low + (high - low) / 2;
+            
+            if(arr[mid] >= arr[mid - 1])
+                low = mid;
+            else
+                high = mid;
+        }
+        
+        return low;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n], tmp[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+			tmp[i] = a[i];
+		}
+		bool f=0;
+		Solution ob;
+		
+		int A = ob. peakElement(tmp,n);
+		
+		if(A<0 and A>=n)
+		    cout<<0<<endl;
+		else
+		{
+    		if(n==1 and A==0)
+    		    f=1;
+    		else if(A==0 and a[0]>=a[1])
+    		    f=1;
+    		else if(A==n-1 and a[n-1]>=a[n-2])
+    		    f=1;
+    		else if(a[A]>=a[A+1] and a[A]>= a[A-1])
+    		    f=1;
+    		else
+    		    f=0;
+    		cout<<f<<endl;
+		}
+		
+	}
+
+	return 0;
+}
+// } Driver Code Ends
